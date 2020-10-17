ef5df45e09a9ebc8236757e3c7a6c6793a56622a
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-17 23:51:28 +0900

    add_\n_\t

--- Myapp.java
+++ Myapp.java
@@ -1,6 +1,22 @@
 public class Myapp {
 
   public static void main(String[]args) {
-    System.out.println("Hello World");
+
+    //文字
+    char a = 'a';
+    //整数　byte short int long
+    int x = 10;
+    long y = 5555555555L;
+    //浮動小数点 float double
+    double d = 23423.344;
+    float f = 32.33F;
+    //論理値
+    boolean flag = true; //false
+    //文字列
+    // \n
+    // \t
+    String msg = "Hello Wo\nrld Again\t!";
+
+    System.out.println(msg);
   }
 }
