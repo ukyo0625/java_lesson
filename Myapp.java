4ec2f440758edef802dc92b6cf53fcbd632c2c2d
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-18 15:31:03 +0900

    add_operator

--- Myapp.java
+++ Myapp.java
@@ -2,21 +2,19 @@ public class Myapp {
 
   public static void main(String[]args) {
 
-    //文字
-    char a = 'a';
-    //整数　byte short int long
-    int x = 10;
-    long y = 5555555555L;
-    //浮動小数点 float double
-    double d = 23423.344;
-    float f = 32.33F;
-    //論理値
-    boolean flag = true; //false
-    //文字列
-    // \n
-    // \t
-    String msg = "Hello Wo\nrld Again\t!";
+    int i;
+    i = 10 / 3;
+    System.out.println(i);//3
 
-    System.out.println(msg);
+    i = 10 % 3;
+    System.out.println(i);//1
+    int x = 5;
+    x++;
+    System.out.println(x);//6
+    x--;
+    System.out.println(x);//5
+
+
+    System.out.println();
   }
 }
