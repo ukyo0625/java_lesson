6318a7782df936425feaf243da80dc8ed807e01a
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-18 15:39:53 +0900

    add_if

--- Myapp.java
+++ Myapp.java
@@ -1,11 +1,13 @@
 public class Myapp {
 
   public static void main(String[]args) {
-
-
-
-   int i = 10;
-   double d = (double)i / 4;
-   System.out.println(d);
+   int score = 85;
+   if (score > 80) {
+    System.out.println("Great!");
+   } else if (score > 60) {
+    System.out.println("Good!");
+   } else {
+    System.out.println("so so ...!");
+   }
   }
 }
