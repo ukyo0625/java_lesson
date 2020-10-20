53628744535a5be58e15e600c6020117b5a4a928
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-20 22:26:14 +0900

    add_array

--- Myapp.java
+++ Myapp.java
@@ -1,12 +1,18 @@
 public class Myapp {
 
   public static void main(String[]args) {
-    for (int i = 0; i < 10; i++) {
-      if (i == 5) {
-        //break;
-        continue;
-      }
-      System.out.println(i);
-    }
+    //配列
+
+    int[] sales;
+    sales = new int[3];
+
+    sales[0] = 100;
+    sales[1] = 200;
+    sales[2] = 300;
+    System.out.println(sales[1]);
+
+    sales[1] = 1000;
+
+    System.out.println(sales[1]);
   }
 }
