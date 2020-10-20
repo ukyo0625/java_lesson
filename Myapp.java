8e35970b3e132b566ade19dee4349e05be6585e4
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-20 22:21:35 +0900

    add_for

--- Myapp.java
+++ Myapp.java
@@ -1,14 +1,12 @@
 public class Myapp {
 
   public static void main(String[]args) {
-    int i = 0;
-    while (i < 10) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 5) {
+        //break;
+        continue;
+      }
       System.out.println(i);
-      i++;
     }
-    do {
-      System.out.println(i);
-      i++;
-    } while (i < 10);
   }
 }
