9b0f6663e1b49b16daaa8148f85dc78d73e53907
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-22 17:43:04 +0900

    add_reference_type

--- Myapp.java
+++ Myapp.java
@@ -1,15 +1,18 @@
 public class Myapp {
 
   public static void main(String[]args) {
-    //配列
+    // int[] a = {3, 5, 7};
+    // int[] b = a;
+    // b[1] = 8;
 
-    int[] sales = {700, 400, 500} ;
+    // System.out.println(a[1]);
+    // System.out.println(b[1]);
 
-    // for (int i = 0; i < 3; i++) {
-    //     System.out.println(sales[i]);
-    // }
-    for (int sale :sales) {
-        System.out.println(sale);
-    }
+    String s = "hello";
+    String t = s;
+    t = "world";
+
+    System.out.println(s);
+    System.out.println(t);
   }
 }
