142ea1bcf05d90dd9c3e3f74f56af6349c795672
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-23 23:18:56 +0900

    add_overload

--- Myapp.java
+++ Myapp.java
@@ -1,20 +1,15 @@
 public class Myapp {
-    //method
 
-    // public static void sayHi() {
-    //     System.out.println("Hi!");
-    // }
-
-    public static String sayHi(String name) {
-        // System.out.println("Hi! " + name);
-        return "Hi! " + name;
-    }
+  public static void sayHi(String name) {
+    int x = 10;
+    System.out.println("Hi! " + name);
+  }
+  public static void sayHi() {
+    System.out.println("Hi! Nobady!");
+  }
 
   public static void main(String[]args) {
-    // sayHi();
-    // sayHi("Tom");
-    // sayHi("Bob");
-    String msg = sayHi("Steve");
-    System.out.println(msg);
+    sayHi();
+    sayHi("Steve");
   }
 }
