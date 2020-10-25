0060e0b0431b3ad7c8424ff50c5fd75304157dc4
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-25 17:03:02 +0900

    add_class

--- Myapp.java
+++ Myapp.java
@@ -1,15 +1,17 @@
+class User {
+  String name = "Me!"; //フィールド
+  void sayHi() {
+    System.out.println("hi!");
+  }
+}
+
+
 public class Myapp {
 
-  public static void sayHi(String name) {
-    int x = 10;
-    System.out.println("Hi! " + name);
-  }
-  public static void sayHi() {
-    System.out.println("Hi! Nobady!");
-  }
-
   public static void main(String[]args) {
-    sayHi();
-    sayHi("Steve");
+    User tom;
+    tom = new User(); //インスタンス
+    System.out.println(tom.name);
+    tom.sayHi();
   }
 }
